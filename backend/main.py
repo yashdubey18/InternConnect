e412fb2b4289fa803f3dfb0d0c57ced0a6ba65e6
Yash <dyash1817@gmail.com>
2025-06-29 20:29:15 +0530

    adding vercel.json

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,9 @@ from database import get_db
 from database import Base  , engine
 import models
 from fastapi.middleware.cors import CORSMiddleware
+import os 
+
+PORT = os.getenv('PORT')
 
 
 app = FastAPI()
@@ -40,4 +43,4 @@ def home():
 
 
 if __name__ =='__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000 , reload = True)
+    uvicorn.run('main:app', host='localhost', port=int(PORT) , reload = True)
